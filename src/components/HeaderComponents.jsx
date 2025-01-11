b0c0d56f8af0bb299a15137fde51f0885e728083
rname97 <risalardan@gmail.com>
2025-01-12 06:36:13 +0700

    update project

--- src/components/HeaderComponents.jsx
+++ src/components/HeaderComponents.jsx
@@ -3,13 +3,9 @@ import React from 'react'
 const HeaderComponents = () => {
   return (
     <>
-        <nav className="navbar bg-body-tertiary">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#">
-                Bootstrap
-                </a>
-            </div>
-        </nav>
+       <header className="header">
+        <h1>Todo App</h1>
+      </header>
       
     </>
   )
--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import HeaderComponents from '../components/HeaderComponents'
 import { createTodo, deleteTodo, getValueById, listTodo, updateTodo } from '../services/todoService';
+import { MdDelete } from "react-icons/md";
+import { MdEdit } from "react-icons/md";
+import { MdBookmarkAdd } from "react-icons/md";
 
 const TodoPage = () => {
+  const [isEdit, setIsEdit] = useState(false);
   const [todos, setTodos] = useState([]);
   const [id, setId] = useState('');
   const [title, setTitle] = useState('');
@@ -14,12 +18,14 @@ const TodoPage = () => {
     status:''
   });
 
-  const [selectedOption, setSelectedOption] = useState('1');
+  // const [selectedOption, setSelectedOption] = useState("");
 
   const handleChange = (event) => {
-    setSelectedOption(event.target.value);
+    console.log("select :"+ event.target.value)
+    setStatus(event.target.value);
   };
 
+  
   useEffect(() => {
     getListAllTodo();
   }, []);
@@ -34,8 +40,11 @@ const TodoPage = () => {
 
   function saveTodo(e){
     e.preventDefault();
+    console.log("proccess save");
     if(validateForm()){
+      console.log("validation save");
       const todoData = {title, description, status}
+      console.log("data todo :"+todoData)
       createTodo(todoData).then((response) => {
         console.log(response.data);
         getListAllTodo();
@@ -51,20 +60,40 @@ const TodoPage = () => {
     setStatus('');
   }
 
-  function getTodoById(todoId){
-    console.log(todoId);
+//   function getTodoById(todoId){
+//     console.log(todoId);
     
-    if(todoId){
-        getValueById(todoId).then((response) => {
-          setId(response.data.id);
-          setTitle(response.data.title);
-          setDescription(response.data.description);
-          setStatus(response.data.status);
-        }).catch(error => {
-            console.error(error); 
-        })
-    }
+//     if(todoId){
+//         getValueById(todoId).then((response) => {
+//           setId(response.data.id);
+//           setTitle(response.data.title);
+//           setDescription(response.data.description);
+//           setStatus(response.data.status);
+//         }).catch(error => {
+//             console.error(error); 
+//         })
+//     }
+// }
+
+
+function openEditModal(todoId){
+  console.log(todoId);
+  
+  if(todoId){
+      getValueById(todoId).then((response) => {
+        setId(response.data.id);
+        setTitle(response.data.title);
+        setDescription(response.data.description);
+        setStatus(response.data.status);
+        setIsEdit(true);
+      }).catch(error => {
+          console.error(error); 
+      })
+  }
 }
+
+
+
 
 function editTodo(e){
     e.preventDefault();
@@ -96,6 +125,7 @@ function delTodo(todoId){
 }
 
 function validateForm(){
+  console.log("proccess validation");
     let valid = true;
     const errorsCopy = {... errors}
     
@@ -113,18 +143,52 @@ function validateForm(){
         valid = false;
     }
 
-    if(status.trim()){
-      errorsCopy.title = '';
-  }else{
-      errorsCopy.title = 'status is required';
+    if (!status || status === "0") {
+      errorsCopy.status = "Please select a valid status.";
       valid = false;
+    }else{
+      errorsCopy.status = '';
+      
   }
+
+  console.log(valid);
+
+  console.log(errorsCopy);
 
     setErrors(errorsCopy);
     return valid;
 }
 
+const resetForm = () => {
+  setId('');
+  setTitle('');
+  setDescription('');
+  setStatus(0);
+  setErrors({});
+  setIsEdit(false);
+};
 
+
+const openAddModal = () => {
+  resetForm();
+  setIsEdit(false);
+};
+
+const getTodoStatus = (statusx) => {
+  console.log("statusx : "+statusx)
+  if (statusx == 1) {
+    console.log("statusx hasil 1: "+statusx)
+    return 'in progress'; // Blue background
+  } else if (statusx == 2) {
+    console.log("statusx hasil 2: "+statusx)
+    return 'done'; // Green background (you can use bg-success instead of bg-warning)
+  } 
+  // else if (status === 'notStarted') {
+  //   return 'bg-warning'; // Yellow background
+  // } else if (status === 'failed') {
+  //   return 'bg-danger'; // Red background
+  // }
+};
 
 
 
@@ -132,42 +196,62 @@ function validateForm(){
     <div>
         <HeaderComponents />
         <div>
-        <div className='m-3'>
+        <div className='m-4'>
             <div className="container">
-                <div className="card border-0">
+                <div className="card" >
+                    
                     <div className="card-body">
                        
-                        
+                    
                         <div className="row justify-content-center">
-                            
-                            <div className="col-sm-7">
+                        
+                            <div className="col-sm-12">
                                 <div className='my-4'>
-                                    <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                                    Add Todo
+                                    <button type="button" className="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal" onClick={openAddModal}>
+                                    <MdBookmarkAdd size={20}/> Add Todo 
                                     </button>
                                 </div>
-                                {
-                                  todos.map(todo => 
-                                    <div className="card my-3" key={todo.id}>
-                                      <div className="card-body">
-                                          <div className="d-flex">
-                                              <div className="flex-grow-1">
-                                                  <h5 className="card-title">{todo.title}</h5>
-                                                  <p className="card-text">{todo.description}</p>
-                                              </div>
-                                              <div className="p-2"><button className='btn btn-primary'  onClick={() => getTodoById(todo.id)} data-bs-toggle="modal" data-bs-target="#exampleModal">Edit</button></div>
-                                              <div className="p-2"><button className='btn btn-danger' onClick={() => delTodo(todo.id)}>Delete</button></div>
-                                          </div>
-                                      </div>
-                                  </div>
-                                  )
-                                }
+                                
+                                <table className="table ">
+                                      <thead>
+                                        <tr>
+                                          <th>No</th>
+                                          <th>Todo Item</th>
+                                          <th>Todo Desc</th>
+                                          <th>Status</th>
+                                          <th>Action</th>
+                                        </tr>
+                                      </thead>
+                                      <tbody>
+                                      {
+                                      todos.map(todo => 
+                                        <tr key={todo.id}>
+                                          <th>1</th>
+                                          <td>{todo.title}</td>
+                                          <td>{todo.description}</td>
+                                          <td>{getTodoStatus(todo.status)}</td>
+                                          <td>
+                                            <div><button className='btn btn-success btn-sm mx-2'  onClick={() => openEditModal(todo.id)} data-bs-toggle="modal" data-bs-target="#exampleModal"><MdEdit size={20}/> Edit</button>
+                                            <button className='btn btn-danger btn-sm' onClick={() => delTodo(todo.id)}><MdDelete size={20}/> Delete</button></div>
+                                          </td>
+                                        </tr>
+                                        )
+                                      }
+                                      </tbody>
+                                    </table>
+
+                               
+                               
+                                    
+                               
+                                  
                                 
                                 
                                 
                             </div>
+                            </div>
                         </div>
-                    </div>
+                  
                 </div>
             </div>
         </div>
@@ -176,7 +260,7 @@ function validateForm(){
             <div className="modal-dialog">
                 <div className="modal-content">
                 <div className="modal-header">
-                    <h1 className="modal-title fs-5" id="exampleModalLabel">Tambah Todo</h1>
+                    <h1 className="modal-title fs-5" id="exampleModalLabel">{isEdit ? 'Edit Todo' : 'Tambah Todo'}</h1>
                     <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div className="modal-body">
@@ -200,10 +284,14 @@ function validateForm(){
                         </div>
                         <div className="mb-3">
                             <label className="form-label">Status</label>
-                            <select className="form-select" aria-label="Default select example"  value={selectedOption} onChange={handleChange}>
-                                <option>Open this select menu</option>
-                                <option value="1">In Progress</option>
-                                <option value="2">Done</option>
+                            <select 
+                              className="form-select" 
+                              aria-label="Default select example"  
+                              value={status} 
+                              onChange={handleChange}>
+                                <option value={0}>Open this select menu</option>
+                                <option value={1}>In Progress</option>
+                                <option value={2}>Done</option>
                             </select>
                             {errors.status && <div className='invalid-feedback'>{errors.status}</div>}
                         </div>
@@ -222,8 +310,8 @@ function validateForm(){
                 </div>
                 <div className="modal-footer">
                     <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="submit" className="btn btn-primary" onClick={saveTodo}>Submit</button>
-                    <button type="submit" className="btn btn-primary" onClick={editTodo}>Update</button>
+                    <button type="submit" className="btn btn-primary" onClick={isEdit ? editTodo : saveTodo}>{isEdit ? 'Update' : 'Submit'}</button>
+                    {/* <button type="submit" className="btn btn-primary" onClick={editTodo}>Update</button> */}
                 </div>
                 </div>
             </div>
